6a4f698369ba94b5474808d24eb0a15945b24ba2
elojah <swingcastor@gmail.com>
2020-02-26 22:34:23 +0100

    wip

--- pkg/wallet/app/wallet.go
+++ pkg/wallet/app/wallet.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"context"
+	"time"
 
 	"github.com/elojah/wallet/pkg/wallet"
 )
@@ -9,9 +10,37 @@ import (
 // App wallet logic implementation.
 type App struct {
 	wallet.Store
+	wallet.TxStore
 }
 
 // ComputeAndFetch logic implementation.
-func (a App) ComputeAndFetch(context.Context, wallet.Filter) ([]wallet.W, error) {
+func (a App) ComputeAndFetch(ctx context.Context, filter wallet.Filter) ([]wallet.W, error) {
+
+	// #Fetch last before date computed wallet
+	w, err := a.Fetch(ctx, wallet.Filter{
+		ID:             filter.ID,
+		LastBeforeDate: filter.StartDate,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// #Fetch all transactions between last computed wallet and end date
+	txs, err := a.FetchManyTx(ctx, wallet.TxFilter{
+		StartDate: time.Unix(w.Timestamp, 0),
+		EndDate:   filter.EndDate,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var result []wallet.W
+	for _, tx := range txs {
+		if time.Unix(tx.Timestamp, 0).After(filter.StartDate) {
+			result = append(result, w)
+		}
+
+	}
+
 	return []wallet.W{}, nil
 }
